956f5980a85517d25111aea344f8d1c23aff6a78
josetelopez <gepettometal@hotmail.com>
2021-05-20 14:23:34 +0200

    Se sube proyecto abeds

---
+++ abedsBackend/src/test/java/com/peretecorporate/AbedsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peretecorporate;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class AbedsBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+//}
